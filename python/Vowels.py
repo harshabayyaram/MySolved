b8a454f1316347b5559c097e3713d85dfa06ead2
Harshath kumar Bayyaram <68186853+harshabayyaram@users.noreply.github.com>
2022-12-15 22:16:20 +0530

    Create Vowels.py

---
+++ python/Vowels.py
@@ -0,0 +1,13 @@
+sentence=input("enter sentence:")
+string=sentence.lower()
+#print(string)
+
+count=0
+list1=["a","e","i","o","u"]
+a=list()
+for char in string:
+  if char in list1:
+    count=count+1
+    a.append(char)
+print(a)
+print("number vowels in given sentence is:",count)
